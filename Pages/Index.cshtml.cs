42b94442c4f6c2210d3a64d5414b6f86ded670e1
krystof <brv@brv.brv>
2024-04-12 15:16:50 +0200

    i  Please enter the commit message for your changes. Lines starting deleted unnecessary things in Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,24 +4,13 @@ namespace TimeCapsuleV3._1.Pages;
 
 public class IndexModel : PageModel {
     private readonly ILogger<RozcestnikModel> _logger;
-    private static readonly string password = "Landsdorfův Pahorek";
 
     public IndexModel(ILogger<RozcestnikModel> logger) {
+        
         _logger = logger;
     }
 
     public void OnGet() {
     }
 
-    public static string getPassword() {
-        return password;
-    }
-
-    public static bool check(string s) {
-        if (s == getPassword()) {
-            return true;
-        }
-
-        return false;
-    }
 }
